dabd09835b5b109f1a0beb7cd6c092c4c7516493
Jalal <mrjalalsayed@gmail.com>
2024-01-16 09:58:39 +0000

    Controlling PWM using analog inputs (joystick) - Close #3

--- testPWM.c
+++ testPWM.c
@@ -12,8 +12,11 @@
 #include "pico/time.h"
 #include "hardware/irq.h"
 #include "hardware/pwm.h"
+#include "hardware/adc.h"
 
-#define PWM_PIN 2
+#define PWM_PIN_1 0
+#define PWM_PIN_2 1
+
 int fade = 0;
 bool going_up = true;
 
@@ -21,59 +24,47 @@ int main()
 {
     // for printf to console:
     stdio_init_all();
-    // Tell the LED pin that the PWM is in charge of its value.
-    gpio_set_function(PICO_DEFAULT_LED_PIN, GPIO_FUNC_PWM);
-    gpio_set_function(PWM_PIN, GPIO_FUNC_PWM);
-    // Figure out which slice we just connected to the LED pin
-    uint slice_num_2 = pwm_gpio_to_slice_num(PICO_DEFAULT_LED_PIN);
-    uint slice_num = pwm_gpio_to_slice_num(PWM_PIN);
+    adc_init();
+    // Make sure the GPIO is high-impedance, no pullups etc:
+    adc_gpio_init(31);
+    adc_gpio_init(32);
 
-    // Mask our slice's IRQ output into the PWM block's single interrupt line,
-    // and register our interrupt handler
-    // pwm_clear_irq(slice_num);
-    // pwm_set_irq_enabled(slice_num, true);
-    // irq_set_exclusive_handler(PWM_IRQ_WRAP, on_pwm_wrap);
-    // irq_set_enabled(PWM_IRQ_WRAP, true);
+    // Tell the LED pin that the PWM is in charge of its value.
+    gpio_set_function(PWM_PIN_1, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_PIN_2, GPIO_FUNC_PWM);
+
+    // Figure out which slice we just connected to the LED pin
+    uint slice_num_1 = pwm_gpio_to_slice_num(PWM_PIN_1);
+    uint slice_num_2 = pwm_gpio_to_slice_num(PWM_PIN_2);
 
     // Get some sensible defaults for the slice configuration. By default, the
     // counter is allowed to wrap over its maximum range (0 to 2**16-1)
     pwm_config config = pwm_get_default_config();
 
+    pwm_init(slice_num_1, &config, true);
     pwm_init(slice_num_2, &config, true);
-    pwm_init(slice_num, &config, true);
 
-    // Everything after this point happens in the PWM interrupt handler, so we
-    // can twiddle our thumbs
     while (1)
     {
-        // tight_loop_contents();
+        adc_select_input(0);
+        // raw adc value between 0 and 2**12-1 (0-4095):
+        uint16_t x_raw = adc_read();
+        adc_select_input(1);
+        uint16_t y_raw = adc_read();
 
-        printf("fade is %d\n", fade);
-        printf("going_up is %d\n", going_up);
+        printf("x_raw is %d\n", x_raw);
+        printf("y_raw is %d\n", y_raw);
 
-        if (going_up)
-        {
-            fade += 10;
-            if (fade > 255)
-            {
-                fade = 255;
-                going_up = false;
-            }
-        }
-        else
-        {
-            fade -= 10;
-            if (fade < 0)
-            {
-                fade = 0;
-                going_up = true;
-            }
-        }
+        // scale to 0-255 (0-2**8-1)):
+        uint8_t x_pos = x_raw >> 4;
+        uint8_t y_pos = y_raw >> 4;
 
-        // Square the fade value to make the LED's brightness appear more linear
-        // Note this range matches with the wrap value
-        pwm_set_gpio_level(PICO_DEFAULT_LED_PIN, fade * fade);
-        pwm_set_gpio_level(PWM_PIN, fade * fade);
+        printf("x_pos is %d\n", x_pos);
+        printf("y_pos is %d\n", y_pos);
+
+        // Square the value to make the transition appear more linear (final value is between 0 and 2**16-1):
+        pwm_set_gpio_level(PWM_PIN_1, x_pos * x_pos);
+        pwm_set_gpio_level(PWM_PIN_2, y_pos * y_pos);
 
         sleep_ms(100);
     }
