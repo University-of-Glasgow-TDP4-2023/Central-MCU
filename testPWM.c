66858944efeba7f32f80d4b39ad51f7c7b60966e
Jalal <mrjalalsayed@gmail.com>
2024-01-16 09:24:34 +0000

    PWM signal successfully outputed from gpio0 - Close #2

--- testPWM.c
+++ testPWM.c
@@ -1,40 +1,80 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+// Fade an LED between low and high brightness. An interrupt handler updates
+// the PWM slice's output level each time the counter wraps.
+
 #include "pico/stdlib.h"
-#include "hardware/gpio.h"
+#include <stdio.h>
+#include "pico/time.h"
+#include "hardware/irq.h"
 #include "hardware/pwm.h"
 
-// Define GPIO pin for PWM
-const uint PWM_PIN = 20;
+#define PWM_PIN 2
+int fade = 0;
+bool going_up = true;
 
-int main() {
-
-    // Steps to initialise PWM:
-    // 1. Claim the GPIO pin for the PWM
-    // 2. Set the GPIO function to PWM
-    // 3. Find the slice number for the GPIO pin
-    // 4. Set the PWM wrap value
-    // 5. Set the PWM clock divider
-    // 6. Enable the PWM slice
-
-    // Initialize GPIO pins
-    gpio_init(PWM_PIN);
-    gpio_set_dir(PWM_PIN, GPIO_OUT);
-
-    // Initialize PWM for speed control
+int main()
+{
+    // for printf to console:
+    stdio_init_all();
+    // Tell the LED pin that the PWM is in charge of its value.
+    gpio_set_function(PICO_DEFAULT_LED_PIN, GPIO_FUNC_PWM);
     gpio_set_function(PWM_PIN, GPIO_FUNC_PWM);
+    // Figure out which slice we just connected to the LED pin
+    uint slice_num_2 = pwm_gpio_to_slice_num(PICO_DEFAULT_LED_PIN);
     uint slice_num = pwm_gpio_to_slice_num(PWM_PIN);
-    pwm_set_wrap(slice_num, 65535);
-    pwm_set_clkdiv(slice_num, 4.f); // Set PWM frequency
-    pwm_set_enabled(slice_num, true);
-    
-    // Test the motor control
-    while (true) {
-        for (int i=0; i<65535; i+=5) {
-            pwm_set_chan_level(slice_num, PWM_CHAN_A, i);
-            sleep_ms(100);
+
+    // Mask our slice's IRQ output into the PWM block's single interrupt line,
+    // and register our interrupt handler
+    // pwm_clear_irq(slice_num);
+    // pwm_set_irq_enabled(slice_num, true);
+    // irq_set_exclusive_handler(PWM_IRQ_WRAP, on_pwm_wrap);
+    // irq_set_enabled(PWM_IRQ_WRAP, true);
+
+    // Get some sensible defaults for the slice configuration. By default, the
+    // counter is allowed to wrap over its maximum range (0 to 2**16-1)
+    pwm_config config = pwm_get_default_config();
+
+    pwm_init(slice_num_2, &config, true);
+    pwm_init(slice_num, &config, true);
+
+    // Everything after this point happens in the PWM interrupt handler, so we
+    // can twiddle our thumbs
+    while (1)
+    {
+        // tight_loop_contents();
+
+        printf("fade is %d\n", fade);
+        printf("going_up is %d\n", going_up);
+
+        if (going_up)
+        {
+            fade += 10;
+            if (fade > 255)
+            {
+                fade = 255;
+                going_up = false;
+            }
         }
+        else
+        {
+            fade -= 10;
+            if (fade < 0)
+            {
+                fade = 0;
+                going_up = true;
+            }
+        }
+
+        // Square the fade value to make the LED's brightness appear more linear
+        // Note this range matches with the wrap value
+        pwm_set_gpio_level(PICO_DEFAULT_LED_PIN, fade * fade);
+        pwm_set_gpio_level(PWM_PIN, fade * fade);
+
+        sleep_ms(100);
     }
-
-    return 0;
-
-
 }
